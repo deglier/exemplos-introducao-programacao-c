b90b24daa329905949c1fe8041ae0e1c343b78c5
Deglier Amorim <degliersa@gmail.com>
2017-03-28 13:34:44 -0300

    first commit

---
+++ variaveis/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int numeroInteiro = 10; // tipo que retorna apenas números reais
+    printf("variavel inteira: %d", numeroInteiro);
+
+    float numeroReal = 5.3; // Números reais em linguagens de programação é usado a notação de . no lugar da virgula de decimais
+    printf("\nvariavel real %f", numeroReal);
+
+    return 0;
+}
